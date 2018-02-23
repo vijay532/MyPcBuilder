a29f35b9ae8e2981ad71ada4ba8d911209d933b2
Asutosh Padhi <2015asutosh.padhi@ves.ac.in>
2018-02-23 22:50:12 +0530

    Include Product Page

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/choose-pc')
+def choosePc():
+    return render_template("choose-pc.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
